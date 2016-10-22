c5c2b0107f0d91fa79799a2859dd4fdde1af67a7
kapatton90 <kapatton19@gmail.com>
2016-10-22 11:31:35 -0500

    Create rails api skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+GoalMate::Application.config.secret_token = '291091772a42dc92f0f217eab179826af37fa375cefe211126ce581562851f9b620ea269a19d952179921a949e517dcf1f921312759b86d22f00cab49112b31c'
