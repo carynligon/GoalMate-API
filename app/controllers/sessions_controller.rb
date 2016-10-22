73675f808dc7d1de4db1c16f3e00aabbb823c022
Jessica Bohn <bohn.16@wright.edu>
2016-10-22 15:35:27 -0500

    Remove manual error
    The create action was manually adding an invalid input error to
    the user's list of erros, but that wasn't necessary since the JSON
    being rendered will automatically include all of the errors that
    have occured for the user. So the manuall addition was removed.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,8 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       session[:id] = @user.id
     else
-      @user.errors << "Invalid email or password"
       render :json { errors: @user.errors }
     end
   end
-  
+
 end
