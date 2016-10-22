51519fe953f6432ed6e7e518407365588996fff6
Jessica Bohn <bohn.16@wright.edu>
2016-10-22 13:15:16 -0500

    Change content type to text
    The content type for posts was string, which limits it to 255
    characters, but that isn't very long for a forum post. Therefore
    the attribute type for a post's content was changed from string to
    text before the migration was run.

--- db/migrate/20161022173120_create_posts.rb
+++ db/migrate/20161022173120_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
     	t.string :type
-    	t.string :content
+    	t.text :content
     	t.timestamps
     end
   end
