35393cb7279af971a7bd413efb9daf5087e7749b
Jessica Bohn <bohn.16@wright.edu>
2016-10-22 14:31:50 -0500

    Update User create action
    The create action for the User model was previously empty and not
    performing any tasks. So the action was updated to now create a new
    instance of a User with the permitted user params. If the instance
    is valid and saved then the page returns a json of the user's
    information. If the user is invalid then the page returns a JSON
    with the current errors and correct status for wrong input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,15 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { errors: @user.errors }, status: :unprocessable_entity
   end
 
   def edit
