81adc244b561c1fe9a43722e8bc97786befd24c3
kapatton90 <kapatton19@gmail.com>
2016-10-22 15:16:49 -0500

    Comment out part of users controller in order to check routes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  def index
-  end
+  # def index
+  # end
 
   def show
     render json: @user
@@ -18,16 +18,17 @@ class UsersController < ApplicationController
       render json: @user
     else
       render json: { errors: @user.errors }, status: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
   private
     def user_params
@@ -37,4 +38,5 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(session[:id])
     end
+
 end
