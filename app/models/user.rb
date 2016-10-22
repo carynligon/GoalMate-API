102ed7ac5d6c7c4e18fb7159c78253c97a027119
Jessica Bohn <bohn.16@wright.edu>
2016-10-22 14:02:05 -0500

    Add method to downcase emails before saving
    Previously user emails would be saved in their original inputted
    format, which would mean that they would forever be case-sensitive,
    which we don't necessarily want. So a method was created to downcase
    all emails before saving them so that they are no longer case-sensitive
    and cause less problems for logging in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
 
   validates_presence_of :email, :first_name, :last_name, :password, :zipcode
   validates_uniqueness_of :email
+
+  before_save :downcase_email
+
+  private
+
+    def downcase_email
+      self.email = email.downcase_email
+    end
+
 end
