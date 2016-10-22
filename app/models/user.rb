1aacc1a0fb3f68445e77a74c052a64de936002a8
Jessica Bohn <bohn.16@wright.edu>
2016-10-22 14:45:56 -0500

    Serialize goals type to Array
    The goals attribute it defined as a string type in the users table,
    because an array is not a direct type for an attribute. So a line
    was added to the model to serialize this attribute into an Array.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  serialize :goals,Array
+  
   validates_presence_of :email, :first_name, :last_name, :password, :zipcode
   validates_uniqueness_of :email
 
