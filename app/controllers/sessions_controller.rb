89510e33d14a7c429f56445bf6f9ec8fab6fc772
Jessica Bohn <bohn.16@wright.edu>
2016-10-22 15:31:07 -0500

    Define Sessions create action
    The application will be using sessions to keep track of which user
    is logged in, but there were no actions defined for the Sessions
    Controller to do so. Therefore the create action was defined for
    Sessions, which finds a user find the email inside of params and
    then checks the password entered against the password saved in the
    database for that user. If the passwords match then the id saved
    within the session is set to that of the user. Else, JSON is rendered
    containing an error saying that the email or password is invalid.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < ApplicationController
 
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+    else
+      @user.errors << "Invalid email or password"
+      render :json { errors: @user.errors }
+    end
+  end
+  
 end
