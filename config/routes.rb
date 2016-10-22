aa64737d054485223296f6db12ba2bbbd0e87bf7
kapatton90 <kapatton19@gmail.com>
2016-10-22 15:02:11 -0500

    Fix merge conflict.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  resources :users
 
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :goals
 end
