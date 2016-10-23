527f3b65023ed66cb00d2ada4196304bdf756cf8
kapatton90 <kapatton19@gmail.com>
2016-10-23 09:32:11 -0500

    Fix files names of models.

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20161023085311) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "type"
-    t.text     "content"
+    t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
